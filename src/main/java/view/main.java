5629ae3e1c7f8bfd76ba54d83eaecacc59615077
Marcos <marcos.lara.0610@fernando3martos.com>
2025-03-27 10:32:40 +0100

    Primer commit
    Signed-off-by: Marcos <marcos.lara.0610@fernando3martos.com>

--- src/main/java/view/main.java
+++ src/main/java/view/main.java
@@ -10,6 +10,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main {
+
+    /*
+    SDFASD
+    ADSFAA
+    AFSDVA
+    FDASDF
+     */
     public static final Scanner S = new Scanner(System.in);
 
     public static void main(String[] args) {
